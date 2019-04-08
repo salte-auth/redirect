9acdbf091dacbc79a6319a8c32ec8d997a590621
Nick Woodward <nick@salte.io>
2019-04-08 18:39:56 -0500

    Updated to support the new Connected API for Handlers

--- test/redirect/redirect.spec.js
+++ test/redirect/redirect.spec.js
@@ -5,10 +5,14 @@ import { Redirect } from '../../src/redirect';
 describe('Redirect', () => {
   /** @type {Redirect} */
   let redirect;
+  /** @type {Promise<string>} */
+  let navigated;
   beforeEach(() => {
     redirect = new Redirect();
     sinon.stub(redirect, 'clear');
-    sinon.stub(redirect, 'navigate');
+    navigated = new Promise((resolve) => {
+      sinon.stub(redirect, 'navigate').callsFake(resolve);
+    });
   });
 
   describe('getter(name)', () => {
@@ -24,98 +28,43 @@ describe('Redirect', () => {
   });
 
   describe('function(connected)', () => {
-    let provider;
-    beforeEach(() => {
-      provider = {
-        validate: sinon.stub(),
-        reset: sinon.stub(),
-        emit: sinon.stub()
-      };
-    });
-
     it('should support logging in', async () => {
       sinon.stub(redirect, 'get').returns('https://google.com');
 
-      const promise = redirect.connected({
-        action: 'login',
-        handler: 'redirect',
-        provider: provider
+      const parsed = redirect.connected({
+        action: 'login'
       });
 
-      expect(provider.validate.callCount).to.equal(0);
       expect(redirect.navigate.callCount).to.equal(0);
+      expect(parsed).to.deep.equal({});
 
-      await promise;
+      await navigated;
 
-      expect(provider.validate.callCount).to.equal(1);
-      expect(provider.reset.callCount).to.equal(0);
       expect(redirect.navigate.callCount).to.equal(1);
     });
 
     it('should support logging out', async () => {
       sinon.stub(redirect, 'get').returns('https://google.com');
 
-      await redirect.connected({
-        action: 'logout',
-        handler: 'redirect',
-        provider: provider
+      const parsed = redirect.connected({
+        action: 'logout'
       });
 
-      expect(provider.reset.callCount).to.equal(1);
-      expect(provider.emit.callCount).to.equal(1);
-      expect(provider.validate.callCount).to.equal(0);
+      expect(redirect.navigate.callCount).to.equal(0);
+      expect(parsed).to.equal(undefined);
+
+      await navigated;
+
       expect(redirect.navigate.callCount).to.equal(1);
-    });
-
-    it(`should throw an error for unknown actions`, async () => {
-      sinon.stub(redirect, 'get').returns('https://google.com');
-
-      try {
-        await redirect.connected({
-          action: 'unknown',
-          handler: 'redirect',
-          provider
-        });
-
-        throw Error('No error was thrown when we expected one.');
-      } catch (error) {
-        expect(error.code).to.equal('unknown_action');
-        expect(provider.reset.callCount).to.equal(0);
-        expect(provider.validate.callCount).to.equal(0);
-        expect(redirect.navigate.callCount).to.equal(0);
-      }
-    });
-
-    it(`should throw an error if a provider isn't given`, async () => {
-      sinon.stub(redirect, 'get').returns('https://google.com');
-
-      try {
-        await redirect.connected({
-          action: 'logout',
-          handler: 'redirect',
-          provider: null
-        });
-
-        throw Error('No error was thrown when we expected one.');
-      } catch (error) {
-        expect(error.code).to.equal('unknown_provider');
-        expect(provider.reset.callCount).to.equal(0);
-        expect(provider.validate.callCount).to.equal(0);
-        expect(redirect.navigate.callCount).to.equal(0);
-      }
     });
 
     it(`should bail if we aren't the active handler`, async () => {
       sinon.stub(redirect, 'get').returns('https://google.com');
 
       await redirect.connected({
-        action: 'login',
-        handler: 'tab',
-        provider
+        action: null
       });
 
-      expect(provider.reset.callCount).to.equal(0);
-      expect(provider.validate.callCount).to.equal(0);
       expect(redirect.navigate.callCount).to.equal(0);
     });
 
@@ -123,13 +72,9 @@ describe('Redirect', () => {
       sinon.stub(redirect, 'get').returns(null);
 
       await redirect.connected({
-        action: 'login',
-        handler: 'redirect',
-        provider
+        action: 'login'
       });
 
-      expect(provider.reset.callCount).to.equal(0);
-      expect(provider.validate.callCount).to.equal(0);
       expect(redirect.navigate.callCount).to.equal(0);
     });
   });
